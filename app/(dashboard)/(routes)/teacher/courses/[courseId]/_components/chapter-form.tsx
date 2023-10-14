4c0fafaa4ecb1adcc2a338b16a1259568012618e
caesar003 <caesarmuksid@gmail.com>
2023-10-14 19:45:21 +0700

    reordered chapters

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
@@ -14,13 +14,14 @@ import {
 } from "@/components/ui/form";
 
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Chapter, Course } from "@prisma/client";
 import { Input } from "@/components/ui/input";
+import ChaptersList from "./chapters-list";
 
 interface ChapterFormProps {
   initialData: Course & { chapters: Chapter[] };
@@ -63,8 +64,30 @@ export default function ChapterForm({
       toast.error("Something went wrong!");
     }
   };
+
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+
+      toast.success("Chapters reordered");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong!");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="mt-6 border bg-slate-100 rounded-md p-4 relative">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button type="button" variant="ghost" onClick={toggleCreating}>
@@ -116,6 +139,11 @@ export default function ChapterForm({
           )}
         >
           {!initialData.chapters.length && "No chapters"}
+          <ChaptersList
+            onEdit={() => {}}
+            onReorder={onReorder}
+            items={initialData.chapters}
+          />
         </div>
       )}
 
